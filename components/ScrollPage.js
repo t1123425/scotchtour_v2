34b343d5113bbad1608cfd20bd64fc358ff9e330
Chris Rice <cd.rice23@gmail.com>
2022-10-31 17:22:46 -0700

    Added some functional scroll animations with framer on test page

--- components/ScrollPage.js
+++ components/ScrollPage.js
@@ -3,13 +3,29 @@ import { useAnimation, motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { Typography, Grid } from "@mui/material";
 import styles from "./ScrollBox.module.css";
+import defaultdata from "../constants/defaultdata";
+import Image from "next/image";
 
 const scrollboxVar = {
-  visible: { opacity: 1, scale: 4, transition: { duration: 1 } },
-  hidden: { opacity: 0, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 0.8,
+      bounce: 0.3,
+      ease: "easeIn",
+    },
+    // filter: "blur(0)",
+  },
+  hidden: {
+    opacity: 0,
+    scale: 0,
+    // filter: "blur(5px)"
+  },
 };
 
-function ScrollBox() {
+function ScrollBox(props) {
   const controls = useAnimation();
   const [ref, inView] = useInView();
   useEffect(() => {
@@ -25,7 +41,18 @@ function ScrollBox() {
       variants={scrollboxVar}
       className={styles.scrollbox}
     >
-      <Typography>TEST!</Typography>
+      <Typography variant="h2" className={styles.scrollboxitem}>
+        {props.defaultdata.header}
+      </Typography>
+      <Image
+        className={styles.scrollboxitem}
+        src={props.defaultdata.imageUrl}
+        width={600}
+        height={400}
+      />
+      <Typography className={styles.scrollboxitem}>
+        {props.defaultdata.text}
+      </Typography>
     </motion.div>
   );
 }
@@ -33,12 +60,27 @@ function ScrollBox() {
 export default function ScrollPage(props) {
   return (
     <>
-      <Grid item>
-        <Typography variant="h2">Scroll Down!</Typography>
-        <ScrollBox />
-        <ScrollBox />
-        <ScrollBox />
-        <ScrollBox />
+      <Grid
+        container
+        item
+        direction="column"
+        alignItems={"center"}
+        rowSpacing={4}
+        justifyContent="center"
+        textAlign={"center"}
+      >
+        <Grid item>
+          <ScrollBox defaultdata={defaultdata} />
+        </Grid>
+        <Grid item>
+          <ScrollBox defaultdata={defaultdata} />
+        </Grid>
+        <Grid item>
+          <ScrollBox defaultdata={defaultdata} />
+        </Grid>
+        <Grid item>
+          <ScrollBox defaultdata={defaultdata} />
+        </Grid>
       </Grid>
     </>
   );
