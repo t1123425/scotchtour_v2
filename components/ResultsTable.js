0c8b378d74bb908a0800373d6a4d245fdc4c51e7
Chris Rice <cd.rice23@gmail.com>
2022-11-07 17:16:49 -0800

    Added some initial deconstruction of components for tables (uncomplete) and added some default samples for a test whisky database service to mimic db calls later.

---
+++ components/ResultsTable.js
@@ -0,0 +1,10 @@
+import { Table } from "@mui/material";
+import React from "react";
+
+export default function ResultsTable(records, headers) {
+  const TableContainer = (props) => <Table></Table>;
+
+  return {
+    TableContainer,
+  };
+}
--- pages/scotch-whisky-search.js
+++ pages/scotch-whisky-search.js
@@ -2,13 +2,21 @@ import React from "react";
 import navItems from "../constants/navitems";
 import DrawerAppBar from "../components/DrawerAppBar";
 import SearchInput from "../components/SearchInput";
+import ResultsTable from "../components/ResultsTable";
+import { TableBody } from "@mui/material";
 
 export default function ScotchDb(pageProps) {
   const title = "Scotch Whisky Search";
+
+  const { TableContainer } = ResultsTable();
+
   return (
     <>
       <DrawerAppBar title={navItems[6].title} />
       <SearchInput />
+      <TableContainer>
+        <TableBody />
+      </TableContainer>
     </>
   );
 }
