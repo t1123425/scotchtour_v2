8a01ac156ec9b79748c27b1601ed445fde230be0
Chris Rice <cd.rice23@gmail.com>
2023-01-04 13:57:39 -0600

    Added static content to return chart stats for regiondata and yeardata on whatis page

--- pages/what-is-scotch-whisky.js
+++ pages/what-is-scotch-whisky.js
@@ -2,11 +2,9 @@ import React from "react";
 import navItems from "../constants/navitems";
 import DrawerAppBar from "../components/DrawerAppBar";
 import ScrollPage from "../components/ScrollPage";
-import { whatIsContent } from "../constants/siteContent";
+import { whatIsContent, yeardata, regiondata } from "../constants/siteContent";
 
 export default function WhatIs(pageProps) {
-  const title = "What is Scotch Whisky?";
-  console.log(whatIsContent);
   return (
     <>
       <DrawerAppBar title={navItems[1].title} />
