236abad1a36b10ac46e126340521e865319802b5
Chris Rice <cd.rice23@gmail.com>
2023-01-04 16:56:09 -0600

    Completed initial writing of formulas. Will extract all chart data into component.

--- pages/visitor-survey.js
+++ pages/visitor-survey.js
@@ -9,6 +9,7 @@ import {
   exampleBrands,
   exampleScotch,
   brandData,
+  regionButtons,
 } from "../constants/siteContent";
 import SubmittedSurvey from "../components/SubmittedSurvey";
 import whiskyDbService from "../services/whiskyDbService";
@@ -56,7 +57,83 @@ export default function VisitorSurvey({ whiskies, surveyResults }) {
   };
   console.log(surveyInput);
   console.log(surveyResults);
-  console.log(surveyResults.map((obj) => obj["scotch-brands"]).flat(1));
+
+  // workspace for chart data
+
+  // Total Survey Responses
+  const totalcount = surveyResults.length;
+  console.log(totalcount);
+
+  // Most Recognized Brands
+  const brandraw = surveyResults.map((obj) => obj["scotch-brands"]).flat(1);
+  const recognizeddata = [...new Set(brandraw)]
+    .map((brand) => ({
+      name: brand,
+      count: brandraw.filter((el) => el === brand).length,
+    }))
+    .sort((a, b) => b.count - a.count)
+    .slice(0, 5);
+
+  // Most Beloved Brands
+  const favoriteraw = surveyResults.map((obj) => obj["favorite-whisky"]);
+  const beloveddata = [...new Set(favoriteraw)]
+    .map((whisky) => ({
+      name: whisky,
+      count: favoriteraw.filter((el) => el === whisky).length,
+    }))
+    .sort((a, b) => b.count - a.count)
+    .slice(0, 5);
+
+  // Most Popular Regions
+  const favwhiskyregions = favoriteraw.map((whisky) => ({
+    name: whisky,
+    region: brandData
+      .filter((el) => new RegExp(el.name).test(whisky))
+      .map((obj) => obj.region)[0],
+  }));
+  const popularregiondata = regionButtons.map((obj) => ({
+    name: obj.name,
+    count: favwhiskyregions.filter((el) => el.region === obj.name).length,
+  }));
+
+  // Scotch vs. other Types of whisky
+  const comparedtoraw = surveyResults.map((obj) => obj["interest-slider"]);
+  const comparedtodata = Object.keys(interestLabels)
+    .sort((a, b) => a - b)
+    .map((el) => ({
+      rating: el,
+      count: comparedtoraw.filter((item) => item === Number(el)).length,
+    }));
+
+  // How People Feel About Scotch
+  const hoverraw = surveyResults.map((obj) => obj["hover-feedback"]);
+  const hoverdata = Object.keys(ratingLabels)
+    .sort((a, b) => a - b)
+    .map((el) => ({
+      rating: el,
+      percent: (
+        (hoverraw.filter((item) => item === Number(el)).length / totalcount) *
+        100
+      ).toFixed(2),
+    }));
+
+  // Best Whisky Notes
+  const tagraw = favoriteraw
+    .map((el) => ({
+      name: el,
+      tags: whiskies
+        .filter((obj) => obj.whisky === el)
+        .map((obj) => obj.tags)[0],
+    }))
+    .map((obj) => obj.tags)
+    .flat(1);
+  const whiskynotedata = [...new Set(tagraw)]
+    .map((el) => ({
+      flavor: el,
+      count: tagraw.filter((item) => item === el).length,
+    }))
+    .sort((a, b) => b.count - a.count);
+  console.log(whiskynotedata);
 
   return (
     <>
