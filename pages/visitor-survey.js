6f4f258c2712c92c9ac0822737e4ad88e9d914e6
Chris Rice <cd.rice23@gmail.com>
2023-01-14 11:19:59 -0600

    Updated getserversideprops to fix prerendering issue.

--- pages/visitor-survey.js
+++ pages/visitor-survey.js
@@ -8,15 +8,30 @@ import {
   initialFormValues,
   brandData,
 } from "../constants/siteContent";
-import whiskyDbService from "../services/whiskyDbService";
+// import whiskyDbService from "../services/whiskyDbService";
 import surveyService from "../services/surveyService";
 import useRouter from "next/router";
 import { Typography } from "@mui/material";
+import axios from "axios";
 
-export async function getStaticProps() {
-  const whiskies = await whiskyDbService.getWhisky_db();
-
-  return { props: { whiskies } };
+export async function getServerSideProps() {
+  const WHISKY_URL = "http://localhost:5001/api/whisky_db";
+  const SURVEY_URL = "http://localhost:5001/api/visitor_survey";
+  const whisky_res = await axios.get(WHISKY_URL);
+  const survey_res = await axios.get(SURVEY_URL);
+  const whiskies = await whisky_res.data;
+  const surveyResults = await survey_res.data;
+  if (!whiskies) {
+    return {
+      notFound: true,
+    };
+  }
+  if (!surveyResults) {
+    return {
+      notFound: true,
+    };
+  }
+  return { props: { whiskies, surveyResults } };
 }
 
 export default function VisitorSurvey({ whiskies }) {
